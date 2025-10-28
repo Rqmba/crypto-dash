cc7a954312c48cb6b0c599b39eee697c81e2c460
Rqmba <ramblack45@gmail.com>
2025-10-28 15:27:40 +0100

    Fetch Coins Details, Display Coin Details, add spinner

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,33 @@
 import { useState, useEffect } from "react";
-const API_URL =
-  "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false";
+import HomePage from "./pages/home";
+import AboutPage from "./pages/about";
+import NotFoundPage from "./pages/not-found";
+import CoinsDetailsPage from "./pages/coin-details";
+import { Routes, Route } from "react-router";
+import Header from "./components/Header";
+
+// const API_URL =
+//   "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false";
+
+const BASE_URL = import.meta.env.VITE_API_BASE;
+const PROXY = import.meta.env.VITE_API_PROXY;
 
 const App = () => {
   const [coins, setCoins] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [limit, setLimit] = useState(10);
+  const [filter, setFilter] = useState("");
+  const [sortBy, setSortBy] = useState("market_cap_desc");
 
   useEffect(() => {
+    const API_URL = PROXY
+      ? `${PROXY}${encodeURIComponent(
+          BASE_URL +
+            `&order=market_cap_desc&per_page=${limit}&page=1&sparkline=false`
+        )}`
+      : `${BASE_URL}&order=market_cap_desc&per_page=${limit}&page=1&sparkline=false`;
+
     const fetchCoins = async () => {
       try {
         const res = await fetch(API_URL);
@@ -23,11 +43,33 @@ const App = () => {
     };
 
     fetchCoins();
-  }, []);
+  }, [limit]);
+
   return (
-    <div>
-      <h1>Crypto Dash</h1>
-    </div>
+    <>
+      <Header />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <HomePage
+              coins={coins}
+              filter={filter}
+              setFilter={setFilter}
+              limit={limit}
+              setLimit={setLimit}
+              sortBy={sortBy}
+              setSortBy={setSortBy}
+              loading={loading}
+              error={error}
+            />
+          }
+        />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/coin/:id" element={<CoinsDetailsPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </>
   );
 };
 
---
+++ src/pages/about.jsx
@@ -0,0 +1,10 @@
+const AboutPage = () => {
+  return (
+    <div className="about">
+      <h1>About Crypto Dash</h1>
+      <p>About</p>
+    </div>
+  );
+};
+
+export default AboutPage;
